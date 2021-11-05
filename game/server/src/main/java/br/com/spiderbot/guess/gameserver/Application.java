43d5c2febc01b62a4e2b43daf4a19660fbe68fd0
LVRodrigues <lvrodriguesline@gmail.com>
2021-11-05 13:41:02 -0300

    Renomear arquivo.

---
+++ game/server/src/main/java/br/com/spiderbot/guess/gameserver/Application.java
@@ -0,0 +1,35 @@
+package br.com.spiderbot.guess.gameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+* Inicialização do servidor REST.
+* <p>
+* Apresenta também a página de documentação no endereço inicial.
+*
+* @author <a href="mailto:lvrodrigues@spiderbot.com.br">Luciano Vieira Rodrigues</a>
+* @since 05/11/2021
+* @author $$Author$$
+* @version $$Revision$$ - $$Date$$
+*/
+@SpringBootApplication
+@Controller
+public class Application {
+
+	/**
+	 * Apresenta a página inicial do aplicativo, com a documentação do projeto.
+	 */
+	@GetMapping(value = "/")
+	public ModelAndView getRoot() {
+		return new ModelAndView("index.html");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
