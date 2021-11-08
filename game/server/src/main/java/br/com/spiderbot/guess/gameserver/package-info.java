53a5f27189e5bc16a54ac474ef09dae9b185fb31
Luciano Vieira Rodrigues <lvrodriguesline@gmail.com>
2021-11-08 09:03:30 -0300

    Teste de subistituição de palavras reservadas.

--- game/server/src/main/java/br/com/spiderbot/guess/gameserver/package-info.java
+++ game/server/src/main/java/br/com/spiderbot/guess/gameserver/package-info.java
@@ -1,9 +1,9 @@
 /**
 * Classes de configuração e inicialização do aplicativo.
 
-* @author <a href="mailto:lvrodrigues@spiderbot.com.br">Luciano Vieira Rodrigues</a>
-* @since 05/11/2021
-* @author $$Author$$
-* @version $$Revision$$ - $$Date$$
+* @author $AuthorName$ &lt;$AuthorEmail$&gt;
+* @since $AuthorDate$
+* @author $CommitterName$ &lt;$CommitterEmail$&gt;
+* @since $CommitterDate$
 */
 package br.com.spiderbot.guess.gameserver;
