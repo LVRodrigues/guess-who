60fa3e706f021d7a61dc68aff1b9a8dcd64c553b
LVRodrigues <lvrodriguesline@gmail.com>
2022-11-18 20:14:50 -0300

    Página inicial é pública.

--- java/guess-who-admin/src/main/java/io/github/lvrodrigues/guess/admin/Security.java
+++ java/guess-who-admin/src/main/java/io/github/lvrodrigues/guess/admin/Security.java
@@ -1,9 +1,11 @@
 package io.github.lvrodrigues.guess.admin;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,9 +24,19 @@ public class Security {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors().disable()
+                .csrf().disable()
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated())
+                    .antMatchers(HttpMethod.GET, "/").permitAll()
+                    .anyRequest().authenticated())
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.debug(true)
+                .ignoring()
+                .antMatchers("/*.css", "/*.js", "/*.yaml", "/favicon.ico");
     }
 }
