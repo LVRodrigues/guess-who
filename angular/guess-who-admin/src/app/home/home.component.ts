9f0c3645f2d90768d570dbf49c808f158ee03451
LVRodrigues <lvrodriguesline@gmail.com>
2022-12-11 20:56:15 -0300

    Filtrar.

--- angular/guess-who-admin/src/app/home/home.component.ts
+++ angular/guess-who-admin/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardsService } from '../common/cards.service';
 import { Card } from '../common/model/card';
 import { Page } from '../common/model/page';
@@ -10,13 +10,22 @@ import { Page } from '../common/model/page';
 })
 export class HomeComponent {
 
-  private name: string;
+  @Input() name: string = '';
 
   cards!: Card[];
   page!: Page;
-  
 
   constructor(private cardsService: CardsService) {
+    this.list();
+  }
+
+  onFilter(event: any) {
+    console.debug('Pesquisar por ', this.name);
+    this.list();
+  }
+
+  clearFilter() {
+    console.debug('Limpando o filtro de pesquisa.');
     this.name = '';
     this.list();
   }
