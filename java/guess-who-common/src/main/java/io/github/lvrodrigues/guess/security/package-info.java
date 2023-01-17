fe192a0ede6cc77cfe0dfb0f26827e99a5480486
LVRodrigues <lvrodriguesline@gmail.com>
2023-01-17 15:22:30 -0300

    Update for next development version

--- java/guess-who-common/src/main/java/io/github/lvrodrigues/guess/security/package-info.java
+++ java/guess-who-common/src/main/java/io/github/lvrodrigues/guess/security/package-info.java
@@ -2,8 +2,8 @@
  * Conjunto de classes para habilitar o módulo de autenticação em servidores REST.
  *
  * @since 16/01/2023
- * @author $Author$
- * @author $Committer$
+ * @author $AuthorName$
+ * @author $CommitterName$
  * @branch $Branch$
  */
 package io.github.lvrodrigues.guess.security;
